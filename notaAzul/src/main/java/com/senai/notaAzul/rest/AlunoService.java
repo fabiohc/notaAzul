2d0e448f1773e8dbb46865ef0f14d10a7070fedc
Fabio <Fabio@fcmil>
2017-09-16 13:10:35 -0300

    Commit serviços rest do Aluno

--- notaAzul/src/main/java/com/senai/notaAzul/rest/AlunoService.java
+++ notaAzul/src/main/java/com/senai/notaAzul/rest/AlunoService.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +17,34 @@ import com.senai.notaAzul.model.Aluno;
 @RestController
 @RequestMapping("/aluno")
 public class AlunoService {
-	
+
 	@Autowired
 	private AlunoDao alunoDao;
-	
-	@RequestMapping(value="/list", method = RequestMethod.GET)
-	public List<Aluno> getAlunos(){
-	List<Aluno> result = new ArrayList<>();
-	alunoDao.findAll().forEach(result::add);
-	return result;
-	
+
+	@CrossOrigin
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public List<Aluno> getAlunos() {
+		List<Aluno> result = new ArrayList<>();
+		alunoDao.findAll().forEach(result::add);
+		return result;
+
 	}
 
+	@CrossOrigin
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Aluno getAluno(@PathVariable int id) {
+		return alunoDao.findOne(id);
+	}
+
+	@CrossOrigin
+	@RequestMapping(method = RequestMethod.POST)
+	public void save(@RequestBody Aluno a) {
+		alunoDao.save(a);
+	}
+
+	@CrossOrigin
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable int id) {
+	alunoDao.delete(id);
+	}
 }
